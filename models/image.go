06e8c111364ff0d6119db2b436918ff74416b714
pomelo072 <2641316287@qq.com>
2021-05-03 21:34:52 +0800

    修改字段

--- models/image.go
+++ models/image.go
@@ -1,8 +1,8 @@
 package models
 
 type Image struct {
-	ID            	int    	`gorm:"primaryKey"`
-	ImageType		string	`gorm:"type:varchar(10)"`
-	FatherID		int		`gorm:"type:int"`
-	ImageURL		string	`gorm:"type:varchar(100)"`
+	ID        uint   `gorm:"primaryKey"`
+	ImageType string `gorm:"type:varchar(10)"`
+	FatherID  uint   `gorm:"type:int"`
+	ImageURL  string `gorm:"type:varchar(100)"`
 }
--- models/integration_apply.go
+++ models/integration_apply.go
@@ -6,12 +6,13 @@ import (
 
 type IntegrationApply struct {
 	gorm.Model
-	IntegrationType     int    		`gorm:"type:int"`
-	ApplyUID    		string 		`gorm:"type:varchar(12)"`
-	ApplyText   		string 		`gorm:"type:varchar(50)"`
-	ApplyType			string		`gorm:"type:varchar(10)"`
-	ApplyLevel			string		`gorm:"type:varchar(10)"`
-	ContestLevel		string		`gorm:"type:varchar(10)"`
-	ApplyNumber 		int    		`gorm:"int"`
-	ApplyStatus 		int    		`gorm:"type:int"`
+	IntegrationType int    `gorm:"type:int"`
+	ApplyUID        string `gorm:"type:varchar(12)"`
+	ApplyText       string `gorm:"type:varchar(50)"`
+	ApplyType       string `gorm:"type:varchar(10)"`
+	ApplyLevel      string `gorm:"type:varchar(10)"`
+	ContestLevel    string `gorm:"type:varchar(10)"`
+	ApplyNumber     int    `gorm:"int"`
+	ResponseReason  string `gorm:"type:varchar(50)"`
+	ApplyStatus     int    `gorm:"type:int"`
 }
