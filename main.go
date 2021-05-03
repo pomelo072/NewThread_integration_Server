083a33a46225a4f5ebcd95dfb519cafb857955ed
pomelo072 <2641316287@qq.com>
2021-05-03 21:35:44 +0800

    Release 1.0

--- main.go
+++ main.go
@@ -1,8 +1,33 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"integration_server/config"
+	"integration_server/handlers"
+	"integration_server/router"
+)
 
 func main() {
 	app := gin.Default()
-	app.Run(":8086")
+	// 跨域中间件
+	app.Use(cors.Default())
+	// 校验中间件
+	app.Use(handlers.JwtVerity)
+	// 用户组
+	user := app.Group("/api/user")
+	router.UserGroup(user)
+	// 管理员组
+	admin := app.Group("/api/admin")
+	router.AdminGroup(admin)
+	// 积分组
+	integration := app.Group("/api/integration")
+	router.IntegrationGroup(integration)
+	// 奖品组
+	award := app.Group("/api/exchange")
+	router.AwardGroup(award)
+	// 图片接口
+	app.GET("/api/img/:imgPath", handlers.DownloadIMG)
+	// 启动服务
+	app.Run(config.Sysconfig.Port)
 }
